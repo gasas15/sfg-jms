b1e5c9572f46fdaec2189c137b4a39a2f252784b
Saimir Gasa <saimir.gasa@gmail.com>
2020-11-23 23:23:42 +0100

    Adding a listener.

---
+++ src/main/java/guru/springframework/jms/listener/HelloMessageListener.java
@@ -0,0 +1,25 @@
+package guru.springframework.jms.listener;
+
+import javax.jms.Message;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.jms.config.JmsConfig;
+import guru.springframework.jms.model.HelloWorldMessage;
+
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message) {
+
+        System.out.println("I got a message");
+        System.out.println(helloWorldMessage);
+    }
+}
