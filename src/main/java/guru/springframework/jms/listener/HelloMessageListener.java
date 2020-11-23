728fdf76696d5f4a904faee972dbf36b1a796071
Saimir Gasa <saimir.gasa@gmail.com>
2020-11-23 23:44:14 +0100

    Send and Receive support.

--- src/main/java/guru/springframework/jms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/jms/listener/HelloMessageListener.java
@@ -1,8 +1,12 @@
 package guru.springframework.jms.listener;
 
+import java.util.UUID;
+
+import javax.jms.JMSException;
 import javax.jms.Message;
 
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,16 +14,32 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.jms.config.JmsConfig;
 import guru.springframework.jms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers,
                        Message message) {
 
-        System.out.println("I got a message");
-        System.out.println(helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCVQUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message) throws JMSException {
+
+        HelloWorldMessage worldMessage = HelloWorldMessage
+            .builder()
+            .id(UUID.randomUUID())
+            .message("World!")
+            .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), worldMessage);
     }
 }
--- src/main/java/guru/springframework/jms/sender/HelloSender.java
+++ src/main/java/guru/springframework/jms/sender/HelloSender.java
@@ -2,9 +2,17 @@ package guru.springframework.jms.sender;
 
 import java.util.UUID;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import guru.springframework.jms.config.JmsConfig;
 import guru.springframework.jms.model.HelloWorldMessage;
@@ -15,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
@@ -30,5 +39,34 @@ public class HelloSender {
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
         System.out.println("Message Sent!");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage
+            .builder()
+            .id(UUID.randomUUID())
+            .message("Hello")
+            .build();
+
+        Message receivedMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCVQUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "guru.springframework.jms.model.HelloWorldMessage");
+
+                    System.out.println("Sending Hello!");
+
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("boom");
+                }
+            }
+        });
+
+        System.out.println(receivedMessage.getBody(String.class));
     }
 }
